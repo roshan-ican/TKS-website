957ea10bce6a446bfe329de369be0b4cf4de7d0d
roshan-ican <roshansahani535@gmail.com>
2022-09-23 17:17:59 +0530

    made the cart component

--- components/Cart.js
+++ components/Cart.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import { Context } from "../context"
 
 const Cart = () => {
@@ -10,29 +10,29 @@ const Cart = () => {
   }
 
   return (
-    <div className="flex items-end justify-start py-6">
-      <ul className="divide-y divide-gray-200">
+    <div className="flex flex-col items-end justify-start bg-gray-50 ">
+ 
+      <ul className="flex flex-col bg-white">
         {Object.entries(cart).map(([key, value]) => {
           return (
             <li key={key} className="flex py-6">
-              <div className="flex w-20 h-20 border border-gray-200 rounded-md">
+              <div className="flex flex-row w-20 h-20 border border-gray-200 rounded-md">
                 <img
                   src={cart[key].imagename}
-                  className="object-contain w-16 h-24 rounded shadow"
+                  className="object-contain w-20 h-20 overflow-auto rounded shadow"
                 />
               </div>
               <div className="flex flex-col flex-1 ml-4">
-                <div>
+                <div className="flex flex-col justify-start items-left ">
                   <div className="flex justify-between text-base font-medium text-gray-900">
-                    <h3>{cart[key].name}</h3>
-                    <p className="ml-5">{cart[key].sellprice}</p>
+                    <h3 className="font-medium">{cart[key].productname}</h3>
+                    <p className="flex flex-row mx-4">
+                      Price: ₹{cart[key].sellprice}
+                    </p>
                   </div>
-                  <p className="mt-1 text-sm text-gray-500">
-                    {cart[key].productname}
-                  </p>
                 </div>
                 <div className="flex items-end justify-between flex-1 text-sm">
-                  <p className="text-gray-500">Qty{cart[key].qty}</p>
+                  <p className="text-gray-500">Qty : {cart[key].qty}</p>
                   <div className="flex">
                     <button
                       className="font-medium text-indigo-600 hover:text-indigo-500"
@@ -53,14 +53,11 @@ const Cart = () => {
           )
         })}
       </ul>
-        <div>
-          <a
-          href="#"
-          className="ml-3 btn btn-primary"
-        >
+      <div className="block py-5">
+        <a href="#" className="ml-3 btn btn-primary">
           Checkout
         </a>
-        </div>
+      </div>
     </div>
   )
 }
